5d5c1f01c69bf2ce03373cc9f3e9e9ae3fc2f986
JurajMarkesic <jmarkesic@tvz.hr>
2021-02-11 14:13:20 +0100

    Revert back to old config.

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,7 +29,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('https://primrose.agency/admin/'),
+  history: createWebHistory(),
   routes,
   // Simulate native-like scroll behavior when navigating to a new
   // route and using back/forward buttons.
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://dashboard-frizerrose.netlify.app/' : '/',
-};

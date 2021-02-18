c8b2e670b722c5efebb935f3c5ee5bc7eb3b9b2f
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2021-02-18 13:48:21 +0100

    dynamic props not working

---
+++ src/components/SomeComp.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li>menu item 1</li>
+    <li>menu item 2</li>
+    <li>menu item 3</li>
+    <li>{{ fruit }}</li>
+  </ul>
+</template>
+<script>
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    fruit: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const fruitName = computed(() => props.fruit);
+    console.log(fruitName);
+
+    return {
+      fruitName,
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="home">
+    <!-- <ul>
+      <li>Home</li>
+      <li>Calendar</li>
+      <li>Dodavanje usluga/radnika</li>
+      <li>Settings</li>
+      <li>Help (faq/tutoriali/contact)</li>
+    </ul>
+    <ul>
+      <li>
+        widget sa statistikom:
+        <ul>
+          <li>broj rezervacija u zadnjem tjednu</li>
+          <li>revenue u zadnjem tjednu</li>
+          <li>next week projection?</li>
+        </ul>
+      </li>
+      <li>upcoming termini - sljedećih 10 termina</li>
+      <li>link na tutoriale/faq/contact</li>
+      <li>activity feed (booking, reschedule, cancel)</li>
+    </ul> -->
+
+    <SomeComp :fruit="Banana" />
+
     <Calendar
       v-model="value"
       :number-of-months="3"
@@ -11,7 +34,7 @@
           class="special-day"
         >{{ slotProps.date.day }}</strong>
         <template v-else>
-          {{ slotProps.date.day }}}
+          {{ slotProps.date.day }}
         </template>
       </template>
     </Calendar>
@@ -21,15 +44,18 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import Calendar from 'primevue/calendar';
+import SomeComp from '../components/SomeComp.vue';
 
 export default defineComponent({
   components: {
+    SomeComp,
     Calendar,
   },
 
   setup() {
     const value = ref(null);
     return {
+      SomeComp,
       Calendar,
       value,
     };

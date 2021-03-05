3f6220ab5da2a1e13a26d56087d246987653cad8
JurajMarkesic <jmarkesic@tvz.hr>
2021-03-05 15:29:15 +0100

    Removed sentry because of appstack errors.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // import '@/assets/scss/main.scss';
 import '@/assets/scss/appstack-overrides.scss';
-import * as Sentry from '@sentry/vue';
-import { Integrations } from '@sentry/tracing';
+// import * as Sentry from '@sentry/vue';
+// import { Integrations } from '@sentry/tracing';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
@@ -13,16 +13,16 @@ if (process.env.VUE_APP_API_ENDPOINT === undefined) {
   throw new DeveloperError('No ENV variable for the api endpoint is present');
 }
 
-if (process.env.NODE_ENV === 'production' && process.env.VUE_APP_SENTRY_DSN !== undefined) {
-  Sentry.init({
-    dsn: process.env.VUE_APP_SENTRY_DSN,
-    integrations: [new Integrations.BrowserTracing()],
+// if (process.env.NODE_ENV === 'production' && process.env.VUE_APP_SENTRY_DSN !== undefined) {
+//   Sentry.init({
+//     dsn: process.env.VUE_APP_SENTRY_DSN,
+//     integrations: [new Integrations.BrowserTracing()],
 
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 0.5,
-  });
-}
+//     // We recommend adjusting this value in production, or using tracesSampler
+//     // for finer control
+//     tracesSampleRate: 0.5,
+//   });
+// }
 
 createApp(App).use(store).use(router)
   .mount('#app');

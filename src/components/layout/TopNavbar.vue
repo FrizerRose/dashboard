10b39f0ef8870df71846b423401fb07c5804d8eb
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2021-04-02 01:38:45 +0200

    add favicons

--- src/components/layout/TopNavbar.vue
+++ src/components/layout/TopNavbar.vue
@@ -11,23 +11,7 @@
         <slot />
       </h3>
       <ul class="navbar-nav navbar-align">
-        <li>
-          <div class="round-button-wrap">
-            <router-link
-              to="/pomoc"
-              class="round-button has-shadow"
-            >
-              <span class="o-background-wrap o-ratio">
-                <span class="o-background is-white" />
-                <span class="o-background is-secondary anim-hover" />
-                <span class="o-front">
-                  <span class="round-button__label">?</span>
-                </span>
-              </span>
-            </router-link>
-          </div>
-        </li>
-        <li class="nav-item dropdown d-flex align-items-center">
+        <li class="nav-item dropdown">
           <a
             class="nav-link dropdown-toggle"
             :class="{
@@ -117,91 +101,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="scss" scoped>
-.round-button-wrap {
-  position: relative;
-  z-index: 1;
-  margin-right: 0.5rem;
-}
-.round-button {
-  display: block;
-  border: 2px solid rgba(0,0,0,0.05);
-  border-radius: 9999px;
-  width: 3rem;
-  overflow: hidden;
-  color: black;
-  font-size: 1.5rem;
-  transition: color 0.2s cubic-bezier(0.4, 0, 0, 1);
-
-  &:hover {
-    color: white;
-  }
-
-  &.has-shadow {
-    box-shadow: 0 0 2rem 0 rgba(0, 0, 0, 0.1);
-  }
-}
-
-.o-background-wrap {
-  position: relative;
-  z-index: 0;
-}
-.o-background {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  top: 0;
-  z-index: -1;
-
-  &.is-white {
-    background-color: white;
-  }
-  &.is-secondary {
-    background-color: #495057;
-    border-radius: 9999px;
-    transition: color 0.2s cubic-bezier(0.4, 0, 0, 1);
-  }
-  &.anim-hover {
-    transform: scale(0);
-    transition: transform 0.2s cubic-bezier(0.4, 0, 0, 1);
-    .round-button:hover & {
-      transform: scale(1);
-    }
-  }
-}
-
-.o-front {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.o-ratio {
-  position: relative;
-  display: block;
-  overflow: hidden;
-
-  &:before {
-    display: block;
-    padding-bottom: 100%; /* [1] */
-    width: 100%;
-    content: "";
-  }
-}
-
-.o-ratio_content,
-  .o-ratio > img,
-  .o-ratio > iframe,
-  .o-ratio > embed,
-  .o-ratio > object {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  // height: 100%;
-}
-</style>
--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,21 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? './'
     : '/',
+  pwa: {
+    name: 'Dolazim.hr',
+    themeColor: '#ffffff',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestPath: './img/icons/site.webmanifest',
+
+    // // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // },
+
+  },
 };

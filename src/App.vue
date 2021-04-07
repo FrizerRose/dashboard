f5e4a46958abdea807000c02245acedd5b8a5f45
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2021-04-07 19:16:43 +0200

    add spinner and not found markup

--- src/App.vue
+++ src/App.vue
@@ -3,62 +3,56 @@
     <router-view
       v-if="selectedCompany"
     />
-    <div v-else>
-      <main :class="{'is-loaded': isMounted, 'c-site-main': true}">
-        <section class="c-section">
-          <div class="o-container">
-            <div class="o-orphan">
-              <div class="o-background-wrap">
-                <div class="o-background -has-shadow -overflow" />
-                <div class="o-background -has-bg -overflow" />
-                <div class="o-background -has-border -overflow -theme-color" />
-                <div class="o-orphan_inner">
-                  <div class="o-row">
-                    <div class="o-row_inner">
-                      <h1 class="c-heading">
-                        Poslovni subjekt nije dostupan.
-                      </h1>
-                    </div>
+    <div
+      v-else
+      class="main d-flex justify-content-center w-100"
+    >
+      <main
+        :class="{'is-loaded': isMounted, 'c-site-main': true}"
+        class="content d-flex p-0 w-100"
+      >
+        <div class="container d-flex flex-column">
+          <div class="row h-100">
+            <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+              <div class="d-table-cell align-middle">
+                <div class="card py-6">
+                  <div class="card-body text-center">
+                    <h1>Poslovni subjekt nije dostupan.</h1>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
+        </div>
       </main>
     </div>
   </div>
 
-  <main
+  <div
     v-else
-    class="
-      c-site-main"
+    class="main d-flex justify-content-center w-100"
   >
-    <section class="c-section">
-      <div class="o-container">
-        <div class="o-orphan">
-          <span class="o-icon o-loader">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M12 2a.91.91 0 01.91.91v3.635a.91.91 0 11-1.82 0V2.91A.91.91
-              0 0112 2zM12 16.546a.91.91 0 01.91.909v3.636a.91.91 0 11-1.82 0v-3.636a.91.91 0 01.91-.91zM4.93 4.93a.91.91 0
-              011.286 0l2.572 2.573a.91.91 0 01-1.285 1.285L4.93 6.216a.91.91 0 010-1.286zM15.212 15.212a.91.91 0 011.285
-              0l2.573 2.572a.91.91 0 01-1.286 1.286l-2.572-2.573a.91.91 0 010-1.285zM2 12a.91.91 0 01.91-.91h3.635a.91.91
-              0 110 1.82H2.91A.91.91 0 012 12zM16.545 12a.91.91 0 01.91-.91h3.636a.91.91 0 010 1.82h-3.637a.91.91 0
-              01-.909-.91zM8.788 15.212a.909.909 0 010 1.285L6.216 19.07a.91.91 0 01-1.286-1.286l2.573-2.572a.91.91 0
-              011.285 0zM19.07 4.93a.91.91 0 010 1.286l-2.573 2.572a.909.909 0 11-1.285-1.285l2.572-2.573a.91.91 0 011.286 0z"
-              />
-            </svg>
-          </span>
+    <main class="content d-flex p-0 w-100">
+      <div class="container d-flex flex-column">
+        <div class="row h-100">
+          <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+            <div class="d-table-cell align-middle">
+              <div class="card py-6">
+                <div class="card-body text-center">
+                  <div
+                    class="spinner-border text-primary me-2"
+                    role="status"
+                  >
+                    <span class="sr-only">Loading...</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
-  </main>
+    </main>
+  </div>
 
   <InitialFlow v-if="!isMobile && !isTutorialFinished && selectedCompany !== null && !isOnAuthPages" />
 </template>
